1c4132e1a05ff61020efe071f9f1db57908d44f7
mattfeng <mattfeng@mit.edu>
2020-08-11 14:35:07 -0400

    fix ordering of footnotes to follow appearance in text

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
 const footnotes = require(`remark-numbered-footnotes`);
+const visit = require('unist-util-visit')
+const _ = require(`lodash`)
+const remove = require(`unist-util-remove`)
 
-module.exports = ({ markdownAST }) =>
-  new Promise(resolve => {
-    footnotes()(markdownAST);
+const reorder = (tree) => {
 
-    return resolve(markdownAST);
-  });
+  let footnoteOrder = {}
+  let count = 0
+
+  let defNodeCopy = {} // copies of definition nodes
+
+  visit(tree, (node, index, parent) => {
+    
+    if (node.type === "footnoteReference") {
+      if (!Object.values(footnoteOrder).includes(node.identifier)) {
+        footnoteOrder[count] = node.identifier
+        count++
+      }
+    }
+
+    if (node.type === "footnoteDefinition") {
+      defNodeCopy[node.identifier] = _.cloneDeep(node)
+    }
+  })
+
+  console.log(defNodeCopy)
+
+  remove(tree, { type: "footnoteDefinition" })
+
+  for (let key in footnoteOrder) {
+    // console.log("key", key)
+    tree.children.push(defNodeCopy[footnoteOrder[key]])
+  }
+}
+
+module.exports = ({ markdownAST }) => {
+  return new Promise(resolve => {
+    reorder(markdownAST)
+
+    footnotes()(markdownAST)
+    return resolve(markdownAST)
+  })
+}
